766ca02abcecf11b92a80591d2d9e38726b8b900
programming6131 <kimutainashon@gmail.com>
2023-02-13 15:16:30 +0300

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+* A reactive, predictable state management solution for flutter
---
+++ lib/reactive.dart
@@ -0,0 +1,7 @@
+library reactive;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
